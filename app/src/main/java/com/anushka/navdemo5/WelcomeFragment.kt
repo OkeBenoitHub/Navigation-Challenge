7b5d011ed98a9729acabd69750945be072329af3
preslybenoit <okebenoithub@gmail.com>
2023-09-12 02:51:46 +0100

    Navigation Challenge

---
+++ app/src/main/java/com/anushka/navdemo5/WelcomeFragment.kt
@@ -0,0 +1,36 @@
+package com.anushka.navdemo5
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.anushka.navdemo5.databinding.FragmentEmailBinding
+import com.anushka.navdemo5.databinding.FragmentWelcomeBinding
+import kotlinx.android.synthetic.main.fragment_welcome.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class WelcomeFragment : Fragment() {
+    private lateinit var binding: FragmentWelcomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        val name = arguments!!.getString("input_name")
+        val email = arguments!!.getString("input_email")
+        binding.nameTextView.text = name
+        binding.emailTextView.text = email
+
+        binding.viewTermsButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_welcomeFragment_to_termsFragment)
+        }
+        return binding.root
+    }
+}
